e93f144a832099c95438742b4c68868c0d75268b
balabili <2042270217@qq.com>
2024-06-18 16:57:53 +0800

    first commit

---
+++ src/main/java/org/example/CausedByEatingApplication.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class CausedByEatingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CausedByEatingApplication.class, args);
+    }
+}
