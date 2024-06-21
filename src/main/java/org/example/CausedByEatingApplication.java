fb4fd84c58af58f8281ae57ac7abd2144a7dc686
abirdlikefish <abirdlikefish@qq.com>
2024-06-21 10:02:28 +0800

    a

--- src/main/java/org/example/CausedByEatingApplication.java
+++ src/main/java/org/example/CausedByEatingApplication.java
@@ -3,9 +3,7 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Hello world!
- */
+
 @SpringBootApplication
 public class CausedByEatingApplication {
     public static void main(String[] args) {
